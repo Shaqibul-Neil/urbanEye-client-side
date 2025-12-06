7ae79c3a84cc33adb963376a0fab25937c0851ed
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-12-06 21:36:59 +0600

    manage users fetch query

--- src/pages/dashboard/admin/ManageCitizens.jsx
+++ src/pages/dashboard/admin/ManageCitizens.jsx
@@ -1,4 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/auth & role/useAxiosSecure";
+
 const ManageCitizens = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["all-users"],
+    queryFn: async () => {
+      const res = axiosSecure.get("/users");
+      console.log(res);
+      return res;
+    },
+  });
   return <div></div>;
 };
 
